da8c59ce35e0264997fb3ddc580d69da99f9667e
NehaParakh1 <nehadugar3@gmail.com>
2023-02-09 19:23:13 +0530

    creating small components

---
+++ src/components/ExpenseDetails.js
@@ -0,0 +1,12 @@
+import './ExpenseDetails.css'
+
+function ExpenseDetails(props){
+    return(
+    <div className='expenses'>
+        <div className='expense-item__description'>
+        <h2>{props.title}</h2></div>
+        <div className='expense-item__price'>Rs.{props.amount}</div>
+    </div>
+    )
+}
+export default ExpenseDetails;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css'
+
 function ExpenseItem(props) {
+
     return (
-    <div className='expense-item'>
-        <div>{props.date.toISOString()}</div>
     
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-        </div>
-        <div className='expense-item__price'>Rs.{props.amount}</div>
-        <div className='expense-item__location'>{props.location}</div>
-        
-</div>
+        <div className='expense-item'>
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <ExpenseDetails title={props.title} amount={props.amount}></ExpenseDetails>
+            <div className='expense-item__location'>{props.location}</div>
+            </div>    
+
    
       
 
