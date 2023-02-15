516ba637d28cbcf51c10c52bd7bb965e7144c859
NehaParakh1 <nehadugar3@gmail.com>
2023-02-15 12:28:03 +0530

    adding filter and key and making dynamic

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,37 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-   
-    const clickHandler=()=>{
-        setTitle('Updated!');
-        console.log(title)
-    }
-    const[amount, setAmount] =useState(props.amount);
-
-    const changeAmount =() =>{
-        setAmount(100);
-        console.log(amount)
-    }
-    
-    const deleteExpense=()=>{
-       alert("are you sure")
-         }
-    
+ 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${amount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}> Change Title</button>
-      <button onClick={deleteExpense} > Delete Expense</button> 
-      <button onClick={changeAmount}> Change Amount</button>  
+       
       </Card>
   );
 }
