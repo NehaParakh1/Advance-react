a9946c64c9e19e14cf876bcb70b508f9ea598e51
NehaParakh1 <nehadugar3@gmail.com>
2023-02-15 12:40:24 +0530

    making the filter year item dynamic

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -10,12 +10,18 @@ const Expenses = (props) => {
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   }
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString()===filteredYear
+  })
+
   return (
     <div>
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} 
       onChangeFilter={filterChangeHandler}/>
-      {props.items.map((expense => <ExpenseItem 
+
+      {filteredExpenses.map((expense => <ExpenseItem 
       key={expense.id}
       title={expense.title}
       amount={expense.amount}
