2ae51dade77b017250ee86eb5282749d93647fb1
NehaParakh1 <nehadugar3@gmail.com>
2023-02-16 11:36:42 +0530

    outputting conditional content

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
@@ -15,24 +15,17 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString()===filteredYear
   })
 
+  
   return (
     <div>
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} 
       onChangeFilter={filterChangeHandler}/>
-
-      {filteredExpenses.map((expense => <ExpenseItem 
-      key={expense.id}
-      title={expense.title}
-      amount={expense.amount}
-      date={expense.date}
-      />
-     ) )}
-      
-      
+    
+     <ExpensesList items={filteredExpenses} />
     </Card>
     </div>
   );
-}
 
+  }
 export default Expenses;
